b76387667823ea0c1af9d29c2cf8161cd1d1ec84
Hamza Stitou <hamzastitou11s@hotmail.com>
2026-02-09 20:58:13 +0100

    feat:  model pour match player et team et class Meta

--- backend/matches/models.py
+++ backend/matches/models.py
@@ -2,16 +2,30 @@ from django.db import models
 
 # Create your models here.
 class Match(models.Model):
-    pass
+    date = models.DateTimeField()
+    league = models.CharField(max_length=100)
+    home_team = models.CharField(max_length=100)
+    away_team = models.CharField(max_length=100)
+    home_score = models.IntegerField()
+    away_score = models.IntegerField()
+    mvp_id = models.IntegerField(default=None, blank=True, null=True)
+    class Meta:
+        db_table = 'match' ## nom de la table en db 
+    
 
 
 class Player(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    team_id = models.IntegerField()
+    class Meta:
+        db_table = 'player' ## nom de la table en db 
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
     league= models.CharField(max_length=100)
     ## logo ?? url ou bien image stocké 
+    class Meta: 
+        db_table = 'team' ## nom de la table en db 
     
 
 
