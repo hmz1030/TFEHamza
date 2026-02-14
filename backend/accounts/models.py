57ae90af25d99cafbe199fcd7fea78fc933e9821
Hamza Stitou <hamzastitou11s@hotmail.com>
2026-02-14 18:40:53 +0100

    feat : model User + badge

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,5 +2,25 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class Badge(models.Model):
+    name = models.CharField(max_length=100)
+    min_rated_match = models.IntegerField()
+    icon = models.URLField(blank=True, default='')
+
+    class Meta:
+        db_table = 'badge'
+
+    def __str__(self):
+        return self.name
+
+
 class User(AbstractUser):
-    pass
+    badge = models.ForeignKey(Badge, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    class Meta:
+        db_table = 'user'
+
+    def __str__(self):
+        return self.username
+    
+
