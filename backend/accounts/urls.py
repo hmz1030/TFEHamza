c1e124e2f58cb7f60da7875d0a59a5e46b032450
Hamza Stitou <hamzastitou11s@hotmail.com>
2026-04-18 00:01:07 +0200

    feat: route favoris

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, MeView, FollowView, UnfollowView, FavoriteClubCreateView, FavoriteClubDeleteView, MyActivityView
+from .views import RegisterView, MeView, FollowView, UnfollowView, FavoriteClubCreateView, FavoriteClubDeleteView, FavoriteClubListView, MyActivityView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -10,7 +10,8 @@ urlpatterns = [
     path('me/activity/', MyActivityView.as_view(), name='my-activity'),
     path('follow/', FollowView.as_view(), name='follow'),
     path('unfollow/<int:followee_id>/', UnfollowView.as_view(), name='unfollow'),
-    path('favorites/', FavoriteClubCreateView.as_view(), name='favorite-create'),
+    path('favorites/', FavoriteClubListView.as_view(), name='favorite-list'),
+    path('favorites/add/', FavoriteClubCreateView.as_view(), name='favorite-create'),
     path('favorites/<int:team_id>/', FavoriteClubDeleteView.as_view(), name='favorite-delete'),
 ]
 #lien ci dessous si j'ai besoin de doc pour simple jwt 
