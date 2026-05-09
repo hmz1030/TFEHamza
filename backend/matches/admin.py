3f03218e6b0f415ce9139cf6b27a2edbef09e0e8
Hamza Stitou <hamzastitou11s@hotmail.com>
2026-05-09 15:49:00 +0200

    chore: admin commentaires

--- backend/matches/admin.py
+++ backend/matches/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Team, Player, Match, MatchPlayer, Rating, Vote, Pronostic
+from .models import Team, Player, Match, MatchPlayer, Rating, Comment, Vote, Pronostic
 
 admin.site.register(Team)
 admin.site.register(Player)
 admin.site.register(Match)
 admin.site.register(MatchPlayer)
 admin.site.register(Rating)
+admin.site.register(Comment)
 admin.site.register(Vote)
 admin.site.register(Pronostic)
