541f05ec706c45eac37bbbfa1c9e5cc382548046
Hamza Stitou <hamzastitou11s@hotmail.com>
2026-05-13 23:17:52 +0200

    feat: stats mon profil api

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -61,11 +61,19 @@ class BadgeSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     badge = BadgeSerializer(read_only=True)
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'badge')
+        fields = ('id', 'username', 'email', 'badge', 'followers_count', 'following_count')
         read_only_fields = fields
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
 
 
 class PublicUserSerializer(serializers.ModelSerializer):
