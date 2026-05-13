41d3eafd2a689f2b722e682f943466721ab90111
Hamza Stitou <hamzastitou11s@hotmail.com>
2026-05-13 23:53:42 +0200

    feat: config media avatars

--- backend/matchnote/settings.py
+++ backend/matchnote/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
